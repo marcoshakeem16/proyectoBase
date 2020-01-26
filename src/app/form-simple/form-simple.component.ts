aabb260f541469291f5ff4fe446094fb74bc35ca
unknown <marcoshakeem16@gmail.com>
2020-01-26 14:09:18 -0430

    Validaciones Predefinidas de Angular

--- src/app/form-simple/form-simple.component.ts
+++ src/app/form-simple/form-simple.component.ts
@@ -9,40 +9,35 @@ import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms'
 export class FormSimpleComponent implements OnInit {
 
   /* Variable para Formulario Simple (FormControl) */
-  name = new FormControl('', Validators.required);
+  nameForm = new FormControl('', Validators.required);
 
   /* Variable para Formulario Multiple (FormGroup) */
   login = new FormGroup({
     username: new FormControl('', Validators.required),
-    pass: new FormControl(''),
+    pass: new FormControl('',[Validators.required,Validators.minLength(4)]),
   })
 
   /* Variable para el Formulario Multiple (FormBuilder) */
   informacion = this.fb.group({
-    nombre:['', Validators.required],
-    apellido:['', Validators.required]
+    nombre:['', [Validators.required, Validators.minLength(4)]],
+    apellido:['', [Validators.required, Validators.maxLength(5)]]
   });
-
-  /* Usuario datos */
-  usuario = {
-    username:"",
-    pass:""
-  };
-
-  /* Informacion */
-  info = {
-    nombre:"",
-    apellido:"",
-  };
-
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
      this.login.valueChanges.subscribe(usuario => {
-       this.usuario = this.login.value;
+      /* console.log(this.username.value);
+      *  console.log(this.login);
+      */
+      /* Suscrito a este evento podemos escuchar todos los cambio que introduce
+      *  el usuario desde el input.
+      */
     });
     this.informacion.valueChanges.subscribe(informacion => {
-      this.info = this.informacion.value;
+      /* Suscrito a este evento podemos escuchar todos los cambio que introduce
+       *  el usuario desde el input.
+      */
+     console.log(this.apellido.errors);
     });
   }
 
@@ -51,13 +46,22 @@ export class FormSimpleComponent implements OnInit {
   }
 
   sendGroup(){
-    console.log("Username: " + this.usuario.username);
-    console.log("Password: " + this.usuario.pass);
+    console.log("Username: " + this.username.value);
+    console.log("Password: " + this.pass.value);
   }
 
   sendGroupBuilder() {
-    console.log("Nombre: " + this.info.nombre);
-    console.log("Apellido: " + this.info.apellido);
+    console.log("Nombre: " + this.nombre.value);
+    console.log("Apellido: " + this.apellido.value);
   }
+
+  /* Permite recuperar el control form en el Template */
+  get name () { return this.nameForm};
+  /* Permite recuperar los control form del FormGroup, en el Template  */
+  get username () { return this.login.get('username')};
+  get pass () { return this.login.get('pass')};
+  /* Permite recuperar los control form del FormBuilder, en el Template */
+  get nombre () { return this.informacion.get('nombre')};
+  get apellido () { return this.informacion.get('apellido')};
 
 }
